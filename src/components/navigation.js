9d77cf843bc9f3bf84837893eece11c3229dd28c
Katie Rose Felten <katiefelten@gmail.com>
2020-04-21 14:50:29 -0400

    :zap: setup theme changer

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -1,9 +1,10 @@
-import React from "react"
-import {Link} from "gatsby"
+import React from 'react';
+import { Link } from 'gatsby';
+import ThemeChanger from '../components/themeChanger';
 
 export default (props) => (
-  <nav className="navigation"> 
-    <Link to="/contact">Contact</Link>
+  <nav className='navigation'>
+    <Link to='/contact'>Contact</Link>
+    <ThemeChanger />
   </nav>
-  
-)
+);
--- src/components/post-link.js
+++ src/components/post-link.js
@@ -1,21 +1,24 @@
-import React from "react"
-import { Link } from "gatsby"
+import React from 'react';
+import { Link } from 'gatsby';
 
 const PostLink = ({ post }) => (
-  <article class="card ">
+  <article className='card'>
     <Link to={post.frontmatter.path}>
       {!!post.frontmatter.thumbnail && (
-        <img src={post.frontmatter.thumbnail} alt={post.frontmatter.title + "- Featured Shot"} />
+        <img
+          src={post.frontmatter.thumbnail}
+          alt={post.frontmatter.title + '- Featured Shot'}
+        />
       )}
     </Link>
     <header>
-      <h2 className="post-title">
-        <Link to={post.frontmatter.path} className="post-link">
+      <h2 className='post-title'>
+        <Link to={post.frontmatter.path} className='post-link'>
           {post.frontmatter.title}
         </Link>
       </h2>
-      <div class="post-meta">{post.frontmatter.date}</div>
+      <div class='post-meta'>{post.frontmatter.date}</div>
     </header>
   </article>
-)
-export default PostLink
+);
+export default PostLink;
