fdabdcdc2596b029f2e3fad456834c66428928c7
Katie Rose Felten <katiefelten@gmail.com>
2020-04-22 14:57:56 -0400

    :zap: updated contact page

--- public/admin/cms.js
+++ public/admin/cms.js
@@ -63,7 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "0381ce3f19f36b62642b";
+/******/ 	var hotCurrentHash = "cfc1c0f9ede34c13d9ff";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,54 +1,56 @@
-import React from "react"
-import Helmet from "react-helmet"
-import { graphql } from 'gatsby'
-import Layout from "../components/layout"
+import React from 'react';
+import Helmet from 'react-helmet';
+import { graphql } from 'gatsby';
+import Layout from '../components/layout';
 
-const ContactPage = ({
-  data: {
-    site
-  },
-}) => {
+const ContactPage = ({ data: { site } }) => {
   return (
     <Layout>
       <Helmet>
         <title>Contact — {site.siteMetadata.title}</title>
-        <meta name="description" content={site.siteMetadata.description} />
+        <meta name='description' content={site.siteMetadata.description} />
       </Helmet>
-      <div className="two-grids -contact">
-        <div className="post-thumbnail" style={{backgroundImage: `url('/assets/alexander-andrews-HgUDpaGPTEA-unsplash.jpg')`, marginBottom: 0}}>
-          <h1 className="post-title">Get in Touch</h1>
+      <div className='two-grids -contact'>
+        <div
+          className='post-thumbnail'
+          style={{
+            backgroundImage: `url('/assets/alexander-andrews-HgUDpaGPTEA-unsplash.jpg')`,
+            marginBottom: 0,
+          }}
+        >
+          <h1 className='post-title'>Get in Touch</h1>
           <p>Let me help you kick start your next project &rarr;</p>
         </div>
         <div>
-          <form className="form-container" action="https://sendmail.w3layouts.com/SubmitContactForm" method="post">
-            <div>
-              <label htmlFor="w3lName">Name</label>
-              <input type="text" name="w3lName" id="w3lName"/>
-            </div>
-            <div>
-              <label htmlFor="w3lSender">Email</label>
-              <input type="email" name="w3lSender" id="w3lSender"/>
-            </div>
-            <div>
-              <label htmlFor="w3lSubject">Subject</label>
-              <input type="text" name="w3lSubject" id="w3lSubject"/>
-            </div>
-            <div>
-              <label htmlFor="w3lMessage">Message</label>
-              <textarea name="w3lMessage" id="w3lMessage"></textarea>
-            </div>
-            <div style={{display: "flex", justifyContent: "flex-end"}}>
-              <input type="submit" className="button -primary" style={{marginRight: 0}} />
-            </div>
-          </form>
+          <p>
+            <span role='img' aria-label='Email'>
+              📧
+            </span>{' '}
+            <a href='mailto:katiefelten@gmail.com'> katiefelten@gmail.com</a>
+          </p>
+          <p>
+            <span role='img' aria-label='Laptop'>
+              💻
+            </span>
+            <a href='https://github.com/katie-rose'> github.com/katie-rose</a>
+          </p>
+          <p>
+            <span role='img' aria-label='Bird'>
+              🐦
+            </span>
+            <a href='https://twitter.com/katierose'>
+              {' '}
+              twitter.com/rosethekatie
+            </a>
+          </p>
         </div>
       </div>
     </Layout>
-  )
-}
-export default ContactPage
+  );
+};
+export default ContactPage;
 export const pageQuery = graphql`
-  query ContactPageQuery{
+  query ContactPageQuery {
     site {
       siteMetadata {
         title
@@ -56,4 +58,4 @@ export const pageQuery = graphql`
       }
     }
   }
-`
+`;
